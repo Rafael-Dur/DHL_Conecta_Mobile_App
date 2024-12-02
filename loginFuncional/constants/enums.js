8dcacaccff340b79a42209ffe20f096641824fc2
ledeloss <levidlsantos@gmail.com>
2024-12-02 19:47:13 -0300

    Se agregan los enums apra sl Flujo de envío

---
+++ loginFuncional/constants/enums.js
@@ -0,0 +1,45 @@
+// constants/enums.js
+
+export const ShipmentType = {
+    Package: 1,
+    Document: 2,
+  };
+  
+  export const ShipmentPackageType = {
+    FamilyPresents: 1,
+    Books: 2,
+    Clothes: 3,
+  };
+  
+  export const ShipmentPackageUnit = {
+    Centimeters: 1,
+    Meters: 2,
+    Inches: 3,
+    Feet: 4,
+  };
+  
+  export const WeightUnit = {
+    Kilograms: 1,
+    Pounds: 2,
+  };
+  
+  export const BoxType = {
+    Box: 1,
+    CarryOn: 2,
+    Envelope: 3,
+  };
+  
+  export const ShippingMethod = {
+    Store: 1,
+    HousePickUp: 2,
+  };
+  
+  export const Banks = {
+    BROU: 1,
+    HSBC: 2,
+    ITAU: 3,
+    SANTANDER: 4,
+    SCOTIABANK: 5,
+    VISA: 6,
+  };
+  
